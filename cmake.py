9cbdff0d15febf17a53b79fdb01479f440e5806f
Christopher Snow <csnow@live.ca>
2026-02-22 22:10:51 -0500

    Update how the python path is fetched

--- cmake.py
+++ cmake.py
@@ -29,7 +29,7 @@ import sys
 import os
 
 PYPPERONI_ROOT = os.path.abspath(os.path.dirname(__file__))
-PYTHON_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'python'))
+PYTHON_ROOT = os.environ.get('PYTHON_ROOT', sys.prefix)
 
 
 class CMakeFileGenerator:
